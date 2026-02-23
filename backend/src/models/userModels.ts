2a8973c558272a182437058510388aa960e004ef
Marcos Vinicius <mvsenko7@gmail.com>
2026-02-23 17:51:05 -0300

    Created user and administrator templates.

---
+++ backend/src/models/userModels.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database';
+
+class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public cpf!: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  cpf: {
+    type: DataTypes.STRING(11),
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  sequelize,
+  tableName: 'users',
+  timestamps: true,
+});
+
+export default User;
